9130f04364a88f6c81d8761d8e5a7f9100c51693
alvarodwi <varoalvaro493@gmail.com>
2019-09-09 10:26:15 +0700

    Initial commit

---
+++ app/src/main/java/com/pedoran/datasiswa_231/ui/Dashboard.java
@@ -0,0 +1,16 @@
+package com.pedoran.datasiswa_231.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pedoran.datasiswa_231.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ app/src/main/java/com/pedoran/datasiswa_231/ui/crud/DetailData.java
@@ -0,0 +1,16 @@
+package com.pedoran.datasiswa_231.ui.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pedoran.datasiswa_231.R;
+
+public class DetailData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_data);
+    }
+}
